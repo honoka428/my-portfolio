a60e5f46f2f5f1983937ee1f8693ac62794225f4
Honoka Watanabe <honokawatanabe@Honokas-MacBook-Air.local>
2020-04-24 17:57:45 +0200

    Implement title page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="Title-page">
-      <div className="App-header">
-        <img src={logo} className="App-header-logo"/> 
-      </div>
+    /* Landing */
+    <div className="titlePage">
+
+      <nav>
+        <img src={logo} className="headerLogo"/> 
+        <ul className="navButtons">
+          <li className="ScrollToWork">My work</li>
+          <li className="ScrollToContact">Contact</li>
+          <li className='ResumeLink'>My resume</li>
+        </ul>
+      </nav>
+
+      <main>
+        <h1 className="titleLight">Hi, I'm </h1>
+        <h1 className="titleBold">Honoka.</h1>
+        <h2 className="titleSubtext">Software Engineer & UX/UI Designer</h2>
+        <p className="titleDescription">Here to help you design and code your great ideas into beautiful products.</p>
+      </main>
+
     </div>
+
+    /* Housie */
+
   );
 }
 
