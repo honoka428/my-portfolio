12805ef81804dff5dcfb4977f2fd18b27716a577
Honoka Watanabe <honokawatanabe@Honokas-MacBook-Air.local>
2020-05-02 13:50:50 +0200

    Improve css stylings

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,16 @@ const scrollToTop = () => { scroll.scrollToTop()}
 const Header = () =>
     <div>
         <img src={logo} className="headerLogo" alt="site logo" onClick={() => {scrollToTop()}}/>
-        <Link className="myWorkButton" activeClass="active" to="myWork" spy={true} smooth={true} offset={0} duration= {500}>
+        <Link id="navButton" className="myWorkButton" activeClass="active" to="myWork" spy={true} smooth={true} offset={0} duration= {500}>
             My work 
         </Link>
-        <Link className="workHistoryButton" activeClass="active" to="workHistory" spy={true} smooth={true} offset={0} duration= {500}>
+        <Link id="navButton" className="workHistoryButton" activeClass="active" to="workHistory" spy={true} smooth={true} offset={0} duration= {500}>
             Work history 
         </Link>
-        <Link className="contactButton" activeClass="active" to="contact" spy={true} smooth={true} offset={0} duration= {500}>
+        <Link id="navButton" className="contactButton" activeClass="active" to="contact" spy={true} smooth={true} offset={0} duration= {500}>
             Contact
         </Link>
-        <Link className="resumeButton" activeClass="active" to="resume" spy={true} smooth={true} offset={0} duration= {500}>
+        <Link id="navButton" className="resumeButton" activeClass="active" to="resume" spy={true} smooth={true} offset={0} duration= {500}>
             My resume
         </Link>
     </div>
