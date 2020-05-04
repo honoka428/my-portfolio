0c26cafa12c354a424ab06d4c27fa15c12695464
Honoka Watanabe <honokawatanabe@Honokas-MacBook-Air.local>
2020-05-04 22:28:53 +0200

    Implement contact page

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import '../App.css';
+import React from 'react'
+import '../App.css'
+import './Contact.css'
+import pattern from '../images/pattern.svg'
+import linkedIn from '../images/linkedIn.svg'
+import github from '../images/github.svg'
+import portrait from '../images/portrait.svg'
 
 const Contact = () =>
     <div className='contact-section' id="contact"> 
+        <img src={pattern} alt="background pattern shapes" className="pattern"/>
+        <img src={portrait} className="portrait" alt="honoka" />
+        <h1 id="header" className="contactHeader">Contact</h1>
+        <p id="contactDescription" className="description1">Interested in working together?</p>
+        <p id="contactDescription" className="description2">I’m currently available for freelance work and full time positions.</p>
+        <button className="hireMeButton"> Hire Me </button>
+        <img src={linkedIn} className="linkedInLogo" alt="linkedIn logo" />
+        <img src={github} className="githubLogo" alt="github logo" />
+    
     </div>
 
 export default Contact
