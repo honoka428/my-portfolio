b093ae4bfe4eab64085b4c78717b66fcbc5a1564
Honoka Watanabe <watanabehonoka428@gmail.com>
2020-05-17 16:24:29 +0200

    fix favicon typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import logo from '../images/logo.svg'
-import {animateScroll as scroll} from 'react-scroll'
+import {Link, animateScroll as scroll} from 'react-scroll'
 
-const scrollToTop = () => { scroll.scrollToTop()}
-
-const scrollToX = x => {scroll.scrollTo(x)}
+const scrollToTop = () => { scroll.scrollToTop() }
 
 const Header = () => {
     
@@ -12,15 +10,15 @@ const Header = () => {
         <div>
             <img src={logo} className="headerLogo" alt="site logo" onClick={() => {scrollToTop()}}/>
             <div id="nav">
-                <div onClick={() => {scrollToX(850)}} id="navButton" className="myWorkButton" style={{textDecoration:"none", color: "white"}}>
+                <Link id="navButton" className="myWorkButton" to="myWork" activeClass="active" spy={true} smooth={true}>
                     My work 
-                </div>
-                <div onClick={() => {scrollToX(2450)}} id="navButton" className="workHistoryButton" style={{textDecoration:"none", color: "white"}}>
+                </Link>
+                <Link id="navButton" className="workHistoryButton" to="workHistory" activeClass="active" spy={true} smooth={true}>
                     Work history 
-                </div>
-                <div onClick={() => {scrollToX(3200)}} id="navButton" className="contactButton" style={{textDecoration:"none", color: "white"}}>
+                </Link>
+                <Link id="navButton" className="contactButton" to="contact" activeClass="active" spy={true} smooth={true}>
                     Contact
-                </div>
+                </Link>
                 <a href="files/HWatanabe_Resume.pdf" target="_blank" rel="noopener noreferrer" style={{textDecoration:"none", color: "white"}} id="navButton" className="resumeButton">
                     My resume
                 </a>
