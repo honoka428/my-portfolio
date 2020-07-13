5ac7b3c123af3687e9686142fbcd9c31bcf514a0
Honoka Watanabe <watanabehonoka428@gmail.com>
2020-07-13 15:31:38 +0200

    Make Footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,15 @@ import React from 'react'
 import '../App.css'
 import './Footer.css'
 import {FiHeart} from 'react-icons/fi';
-import {Link as ScrollLink} from 'react-scroll'
 import {Link} from 'react-router-dom';
 
 
 const Footer = () =>
     <div id='footer' className="footerWrapper"> 
         <div id="footerNav">
-            <ScrollLink className="footerItem" to="contact" activeClass="active" spy={true} smooth={true}>
-                Contact
-            </ScrollLink>
+            <Link className="footerItem" to="/">
+                Home
+            </Link>
             <Link className="footerItem" to="aboutHonoka" >
                 About
             </Link>                 
@@ -21,7 +20,7 @@ const Footer = () =>
         </div>            
 
         <div>
-            <p id="madefrom">Made from scratch<FiHeart/> </p>
+            <p id="madefrom">Made from scratch <FiHeart/> </p>
             <p id="copyright"> Copyright © 2020 Honoka Watanabe </p>    
         </div>
     
