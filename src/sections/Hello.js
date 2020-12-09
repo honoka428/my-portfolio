0569a6380fbcdc42bfcab8683c2c22a9803c6b0a
Honoka Watanabe <watanabehonoka428@gmail.com>
2020-12-09 14:46:34 -0500

    Improve look of text loop

--- src/sections/Hello.js
+++ src/sections/Hello.js
@@ -4,7 +4,6 @@ import CodeLinks from '../components/CodeLinks'
 import Lottie from 'react-lottie'
 import matchaAnimation from '../lottie/matcha.json'
 import scrollDownAnimation from '../lottie/scrollDown.json'
-import { useState } from 'react'
 import TextLoop from "react-text-loop";
 
 
@@ -30,11 +29,12 @@ const Body = () => {
 
     return(
         <div style={{textAlign: 'center', paddingTop: '17%', fontSize: '1.5em'}}>
-        <TextLoop interval={2500} mask={true}>
-            <span>Hi</span>
-            <span>I'm Honoka</span>
-            <span>From Japan</span>
-            <span>Web Designer & Developer</span>
+        <span>Hi, I'm </span> {" "}
+        <TextLoop interval={2000} mask={true}>
+            <span style={{fontWeight: '600', color: '#94C7E4'}}>Honoka</span>
+            <span>from <span style={{fontWeight: '600', color: '#94C7E4'}}>Japan</span></span>
+            <span>a <span style={{fontWeight: '600', color: '#94C7E4'}}>designer</span></span>
+            <span>a <span style={{fontWeight: '600', color: '#94C7E4'}}>developer</span></span>
         </TextLoop>
 
         <Lottie 
@@ -46,7 +46,7 @@ const Body = () => {
             options={scrollDownOptions}
             height={100}
             width={200}
-            style={{paddingTop: '10em'}}
+            style={{paddingTop: '9em'}}
         />
     </div>
     )
