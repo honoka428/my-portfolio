0fb4450fc673ab856e62c0991f81a30a0861de68
Honoka Watanabe <watanabehonoka428@gmail.com>
2020-12-09 16:18:56 -0500

    SocialLinks for dribbble and github

--- src/components/CodeLinks.js
+++ src/components/CodeLinks.js
@@ -9,21 +9,15 @@ const CodeLinks = () => {
         display: 'grid',
         templateGridRow: '1fr 1fr',
         height: '10vh',
-        padding: 0,
+        paddingRight: 60,
         cursor: 'pointer',
         listStyleType: 'none'
     }
 
     return(
-        <ul style={styles}>
-            <li>
-                <div className="display-on-hover">Site Source Code</div>
-                <Code id="icon"/>
-            </li>
-            <li>
-                <div className="display-on-hover">Dribbble</div>
-                <Dribbble id="icon"/>
-            </li>
+        <ul style={styles} >
+            <a href="https://github.com/honoka428/my-portfolio" target="_blank"><Code id="icon"/></a>
+            <a href="https://dribbble.com/honokawatanabe" target="_blank"><Dribbble id="icon"/></a>
         </ul>
     )
 }
